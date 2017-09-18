56e4be35df814644ed9ff5ba5baeb885c80f4a3f
BookOwl <stanleybookowl@gmail.com>
2017-09-18 15:07:10 -0400

    Make the example actually do something

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,20 +2,34 @@ extern crate doodle;
 use doodle::*;
 
 #[derive(Default)]
-struct State {}
+struct State {
+    change: i16,
+    color: i16
+}
 
 fn main() {
     let mut doodle = DoodleBuilder::new()
             .name("Test")
             .width(600)
             .height(400)
-            .fps(30)
-            .state(State {})
-            .setup(Box::new(|_, r| {
-                r.set_draw_color(Color::RGB(100, 100, 100));
-                r.clear();
+            .fps(60)
+            .state(State {
+                change: 1,
+                color: 0,
+            })
+            .setup(Box::new(|_, _| {
             }))
-            .draw(Box::new(|_, r| {
+            .draw(Box::new(|s, r| {
+                let c = s.color as u8;
+                s.color += s.change;
+                if s.color > 255 {
+                    s.color = 255;
+                    s.change = -1;
+                } else if s.color < 0 {
+                    s.color = 0;
+                    s.change = 1;
+                }
+                r.set_draw_color(Color::RGB(c, c, c));
                 r.clear();
             }))
             .build()
